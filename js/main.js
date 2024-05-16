9d965cd3a866af0466d87acc459a06b22a333c08
LucasPeraltaa <lucasmperaltaa@gmail.com>
2024-05-16 20:37:47 -0300

    mejoramos nuestro codigo creando una funcion para ahorrar codigo y facilitar el trabajo de cambiar color de fondo y las imagenes

--- js/main.js
+++ js/main.js
@@ -2,17 +2,21 @@ const html = document.querySelector('html')
 const btnEnfoque = document.querySelector('.app__card-button--enfoque')
 const btnCorto = document.querySelector('.app__card-button--corto')
 const btnLargo = document.querySelector('.app__card-button--largo')
+const banner = document.querySelector('.app__image')
 
 btnEnfoque.addEventListener('click', ()=>{
-    html.setAttribute('data-contexto', 'enfoque')
+    cambiarContexto('enfoque')
 })
 
 btnCorto.addEventListener('click', ()=>{
-    html.setAttribute('data-contexto', 'descanso-corto')
+    cambiarContexto('descanso-corto')
 })
 
 btnLargo.addEventListener('click', ()=>{
-    html.setAttribute('data-contexto', 'descanso-largo')
+    cambiarContexto('descanso-largo')
 })
 
-
+function cambiarContexto(contexto){
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src',`./imagenes/${contexto}.png`)
+}
