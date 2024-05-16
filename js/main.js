d777e4171d934b50d189f1933666c03e80c53ea3
LucasPeraltaa <lucasmperaltaa@gmail.com>
2024-05-16 20:44:56 -0300

    a la funcion le agregamos un switch que nos permite cambiar el titulo

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const btnEnfoque = document.querySelector('.app__card-button--enfoque')
 const btnCorto = document.querySelector('.app__card-button--corto')
 const btnLargo = document.querySelector('.app__card-button--largo')
 const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
 
 btnEnfoque.addEventListener('click', ()=>{
     cambiarContexto('enfoque')
@@ -19,4 +20,24 @@ btnLargo.addEventListener('click', ()=>{
 function cambiarContexto(contexto){
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src',`./imagenes/${contexto}.png`)
+    
+    switch (contexto) {
+        case "enfoque":
+            titulo.innerHTML = `Optimiza tu productividad,<br>
+            <strong class="app__title-strong">sumérgete en lo que importa.</strong>`
+            
+            break;
+        case "descanso-corto":
+            titulo.innerHTML = `¿que tal un respirito?
+            <strong class="app__title-strong">haz una pausa corta!!</strong>`
+            break;
+
+        case "descanso-largo":
+            titulo.innerHTML = `A volver al mundo real
+            <strong class="app__title-strong">haz una pausa larga!!</strong>`
+            break;
+
+        default:
+            break;
+    }
 }
